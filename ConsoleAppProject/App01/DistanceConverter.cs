6a3edae6583efb4e6079b9b4ed3f43c28c025901
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2023-02-03 15:06:20 +0000

    week1

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -10,5 +10,61 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+        public static double FEET_IN_MILE = 5280;
+        public static double METER_IN_MILE = 1609.34;
+
+        public double miles, meters, feet;
+
+        public void Run()
+        {
+            OutputHeading();
+
+            InputMiles();
+            CalculateFeet();
+            OutputFeet();
+
+            InputFeet();
+            CalculateMiles();
+            OutputMiles();
+
+
+        }
+
+        private void OutputMiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CalculateMiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void InputFeet()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OutputFeet()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CalculateFeet()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void InputMiles()
+        {
+            Console.Write("Please enter the number of miles ...");
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value); 
+        }
+
+        private void OutputHeading()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

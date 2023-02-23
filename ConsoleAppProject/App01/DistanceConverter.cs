02edbb380367231c86a126710645ca6f373037aa
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2023-02-23 13:58:09 +0000

    new changes yeayyy

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,5 +1,6 @@
 ﻿using ConsoleAppProject.App01;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ConsoleAppProject.App01
 {
@@ -21,6 +22,7 @@ namespace ConsoleAppProject.App01
         string toUnit;
         public void Run()
         {
+            OutputHeading();
             Console.WriteLine("Select the unit you would like to convert...");
             fromUnit = FromUnitMenu();
             Console.WriteLine("Select the unit you would like to convert it to...");
@@ -28,6 +30,7 @@ namespace ConsoleAppProject.App01
 
             fromDis = InputDistance(fromUnit);
             ConvertDis();
+            Output();
         }
 
 
@@ -35,67 +38,82 @@ namespace ConsoleAppProject.App01
 
         private void OutputHeading()
         {
-            Console.Write("Distance Converter By Entwan Enkhbayar");
+            Console.WriteLine("Distance Converter By Entwan Enkhbayar");
+            Console.WriteLine("\t");
         }
 
         public string FromUnitMenu()
         {
-            Console.WriteLine("Which unit would you like to convert ?");
             Console.WriteLine("1. Miles");
             Console.WriteLine("2. Meters");
             Console.WriteLine("3. Feet");
+            string unit;
 
-            string unitSt = Console.ReadLine();
-            int unit = 0;
-
-            if (!int.TryParse(unitSt, out unit) || (unit < 1 || unit > 6))
+            do
             {
-                Console.WriteLine("Please enter valid unit choice from 1 to 3.");
-               
+                Console.WriteLine("Choose the unit of measurement > ");
+                unit = Console.ReadLine();
+                if (unit == "1" || unit.ToLower() == "miles")
+                {
+                    return "miles";
+                }
+                else if (unit == "2" || unit.ToLower() == "meters")
+                {
+                    return "meters";
+                }
+                else if (unit == "3" || unit.ToLower() == "feet")
+                {
+                    return "feet";
+                }
+                if(unit != "1" || unit != "2" || unit != "3" || unit != "4" || unit != "5" || unit != "6" ||
+              unit != "miles" || unit != "meters" || unit != "feet")
+                {
+                    Console.WriteLine("Invalid unit, try again!");
+                }
             }
-            if (unit == 1)
-            {
-                return "miles";
-            }
-            else if (unit == 2)
-            {
-                return "meters";
-            }
-            else if (unit == 3)
-            {
-                return "feet";
-            }
+            while
+            (
+              unit != "1" || unit != "2" || unit != "3" || unit != "4" || unit != "5" || unit != "6" ||
+              unit != "miles" || unit != "meters" || unit != "feet"
+            );
+            
+         
             return null;
         }
 
 
-        public string ToUnitMenu() 
+        public string ToUnitMenu()
         {
-                Console.WriteLine("Which unit would you like to convert the distance to ?");
-                Console.WriteLine("1. Miles");
-                Console.WriteLine("2. Meters");
-                Console.WriteLine("3. Feet");
-
-                string convertSt = Console.ReadLine();
-                int convert = 0;
-
-                if (!int.TryParse(convertSt, out convert) || (convert < 1 || convert > 6))
-                {
-                    Console.WriteLine("Please enter valid unit choice from 1 to 3.");
-                
-                }
-                if (convert == 1)
+            Console.WriteLine("Choose the unit of measurement ");
+            Console.WriteLine("1. Miles");
+            Console.WriteLine("2. Meters");
+            Console.WriteLine("3. Feet");
+            string convert;
+            do
+            {
+                Console.WriteLine("Choose the unit of measurement > ");
+                convert = Console.ReadLine();
+                if (convert == "1" || convert.ToLower() == "miles")
                 {
                     return "miles";
                 }
-                else if (convert == 2)
+                else if (convert == "2" || convert.ToLower() == "meters")
                 {
                     return "meters";
                 }
-                else if (convert == 3)
+                else if (convert == "3" || convert.ToLower() == "feet")
                 {
                     return "feet";
                 }
+                if((convert != "1" || convert != "2" || convert != "3" || convert != "4" || convert != "5" || convert != "6" ||
+               convert != "miles" || convert != "meters" || convert != "feet"))
+                {
+                    Console.WriteLine("Invalid unit, try again!");
+                }
+            }
+            while (convert != "1" || convert != "2" || convert != "3" || convert != "4" || convert != "5" || convert != "6" ||
+               convert != "miles" || convert != "meters" || convert != "feet");
+               
             return null;
         }
 
@@ -108,37 +126,51 @@ namespace ConsoleAppProject.App01
 
         public void ConvertDis()
         {
-            if (fromUnit == "miles" || toUnit == "meters")
+            if (fromUnit == "miles" && toUnit == "meters")
             {
                 toDis = fromDis * METERS_IN_MILE;
             }
-            if (fromUnit == "miles" || toUnit == "feet")
+            else if (fromUnit == "miles" && toUnit == "miles")
+            {
+                toDis = fromDis;
+            }
+            else if (fromUnit == "miles" && toUnit == "feet")
             {
                 toDis = fromDis * FEET_IN_MILE;
             }
-            if (fromUnit == "meters" || toUnit == "miles")
+            else if (fromUnit == "meters" && toUnit == "miles")
             {
                 toDis = fromDis / FEET_IN_MILE;
             }
-            if (fromUnit == "meters" || toUnit == "feet")
+            else if (fromUnit == "meters" && toUnit == "meters")
+            {
+                toDis = fromDis;
+            }
+            else if (fromUnit == "meters" && toUnit == "feet")
             {
                 toDis = fromDis / METERS_IN_FEET;
             }
-            if (fromUnit == "feet" || toUnit == "miles")
+            else if (fromUnit == "feet" && toUnit == "miles")
             {
                 toDis = fromDis / FEET_IN_MILE;
             }
-            if (fromUnit == "feet" || toUnit == "meters")
+            else if (fromUnit == "feet" && toUnit == "feet")
+            {
+                toDis = fromDis;
+            }
+            else if (fromUnit == "feet" && toUnit == "meters")
             {
                 toDis = fromDis / METERS_IN_FEET;
             }
-
         }
-
-
-        }
-
+            public void Output() 
+            {
+            Console.WriteLine(fromDis + " "+ fromUnit + " is " + toDis + " " + toUnit + " !" );
+            }
     }
+}
+
+    
 
 
 
